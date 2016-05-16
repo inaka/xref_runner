b84c9202f33dd1ff89c8d951e3abb3fc2c63ef04
Euen <euenlopez@gmail.com>
2016-05-16 14:27:09 -0300

    [#56] create tools_meta_SUITE with Elvis & Dialyzer

--- test/xref_meta_SUITE.erl
+++ test/xref_meta_SUITE.erl
@@ -1,18 +1,43 @@
 -module(xref_meta_SUITE).
+-author('euen@inaka.net').
 
--include_lib("mixer/include/mixer.hrl").
--mixin([{ktn_meta_SUITE
-        , [ dialyzer/1
-          , elvis/1
-          ]
-        }]).
-
--export([all/0, init_per_suite/1]).
+-export([all/0]).
+-export([dialyzer/1, elvis/1]).
 
 -type config() :: [{atom(), term()}].
 
 -spec all() -> [dialyzer | elvis].
 all() -> [dialyzer, elvis].
 
--spec init_per_suite(config()) -> config().
-init_per_suite(Config) -> [{application, xref_runner} | Config].
+-spec dialyzer(config()) -> {comment, []}.
+dialyzer(_Config) ->
+  BaseDir = code:lib_dir(xref_runner),
+  DefaultRebar3PltLoc = filename:join(BaseDir, "../../../default"),
+  Plts = filelib:wildcard(filename:join(DefaultRebar3PltLoc, "*_plt")),
+  Dirs = [filename:join(BaseDir, Dir) || Dir <- ["ebin", "test"]],
+  Warnings = [error_handling, race_conditions, unmatched_returns],
+  ct:comment("Dialyzer must emit no warnings"),
+  Opts =
+    [ {analysis_type, succ_typings}
+    , {plts,          Plts}
+    , {files_rec,     Dirs}
+    , {check_plt,     true}
+    , {warnings,      Warnings}
+    , {get_warnings,  true}
+    ],
+  [] = [dialyzer:format_warning(W, basename) || W <- dialyzer:run(Opts)],
+  {comment, ""}.
+
+-spec elvis(config()) -> {comment, []}.
+elvis(_Config) ->
+  BaseDir = code:lib_dir(xref_runner),
+  ConfigFile = filename:join(BaseDir, "../../../../elvis.config"),
+  ElvisConfig = [ fix_dirs(Group)
+                || Group <- elvis_config:load_file(ConfigFile)],
+  ct:comment("Elvis rocks!"),
+  ok = elvis_core:rock(ElvisConfig),
+  {comment, ""}.
+
+fix_dirs(#{dirs := Dirs} = Group) ->
+  NewDirs = [filename:join(code:lib_dir(xref_runner), Dir) || Dir <- Dirs],
+  Group#{dirs := NewDirs}.
